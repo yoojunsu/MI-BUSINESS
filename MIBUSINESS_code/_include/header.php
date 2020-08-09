987884ac1246e781538e065b11234be7f57bb556
yoojunsu <vcoavmv@naver.com>
2020-08-10 04:44:13 +0900

    MIBUSINESS_first

---
+++ MIBUSINESS_code/_include/header.php
@@ -0,0 +1,46 @@
+<div class="header-title-wrap">
+	<h1>
+		<a href="#none">
+			MI BUSINESS
+		</a>
+	</h1>
+</div>
+<div class="hamburger-menu-wrap">
+	<span class="line top"></span>
+	<span class="line mid"></span>
+	<span class="line bottom"></span>
+</div>	
+<div class="header-nav-wrap">
+	<ul class="nav">
+		<li>
+			<a href="http://genesis.popol.website/">HOME</a>
+		</li>
+		<li>
+			<a href="#none" id="popol-menu">
+				CONTENT-Portpolio
+				<span>+</span>
+			</a>
+			<ul class="sub-nav">
+				<li>
+					<a href="#none">GOOD TRAVEL</a>
+				</li>
+				<li>
+					<a href="#none">MI BUSINESS</a>
+				</li>
+				<li>
+					<a href="#none">Catch Digda!</a>
+				</li>
+				<li>
+					<a href="#none">KAKAO FRIEND</a>
+				</li>
+				<li>
+					<a href="#none">PORTPOLIO SITE</a>
+				</li>
+			</ul>
+		</li>
+		<li>
+			<a href="#none">CONTENT-Working</a>
+		</li>
+	</ul>
+</div>
+
